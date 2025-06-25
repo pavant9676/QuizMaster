666ebed71c6b354f1c8dace38b281f7ad6bf1b73
pavant9676 <pavankumar.style369@gmail.com>
2025-06-25 23:01:52 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\PAVAN KUMAR\Downloads\onlinequiz-master\onlinequiz-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
